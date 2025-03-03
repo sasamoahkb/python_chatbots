f9fffc3deb63df02a5bd4a8b134c5704c500e8e4
Samuel K. Asamoah <asamoahs@roehampton.ac.uk>
2025-03-03 12:56:33 +0000

    my first commit

--- assistant.py
+++ assistant.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 
 client = OpenAI(
-    api_key = "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")
+    api_key = "PRIVATE")
 
 messages = []
 def getAssistantType():
